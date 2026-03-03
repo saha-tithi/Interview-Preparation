10ce2316e053e15cf20eff1aa6b6e7c4a726daf3
saha-tithi <email.tithisaha@gmail.com>
2026-03-03 13:25:46 +0530

    Day 8

---
+++ Basic practice/2ndLarge.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number of elements"))
+my_list=[]
+for i in range(n):
+    element=input()
+    my_list.append(element)
+my_list.sort
+my_set=list(map(set, my_list))
+my_set=list(my_set)
+print(my_set[1])
---
+++ TAKEUFORWARD/reverse_ll.py
@@ -0,0 +1,19 @@
+# Definition of singly linked list:
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        temp=head
+        prev=None
+        while(temp!=None):
+            front=temp.next
+            temp.next=prev
+            prev=temp
+            temp=front
+        return prev
+
+
+
