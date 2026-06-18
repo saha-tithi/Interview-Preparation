fc54d43934d7199350992457d26fbd360960e225
saha-tithi <email.tithisaha@gmail.com>
2026-06-18 19:59:52 +0530

    day 13

---
+++ TAKEUFORWARD/min_coin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def MinimumCoins(self, coins, amount):
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for i in range(1,(amount+1)):
+            for coin in coins:
+                if i>=coin:
+                    dp[i]=min(dp[i],dp[i-coin]+1)
+        if dp[amount]==float('inf'):
+            return -1
+        else:
+            return dp[amount]
+    
