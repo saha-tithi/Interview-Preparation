f6060a475689ab1b4aae7bff00bac197902273f6
saha-tithi <email.tithisaha@gmail.com>
2026-07-27 19:19:04 +0530

    practice

--- TAKEUFORWARD/missing_reaoiting.py
+++ TAKEUFORWARD/missing_reaoiting.py
@@ -1,3 +1,4 @@
+#mathematic approach
 class Solution:
     def findMissingRepeatingNumbers(self, nums):
         n=len(nums)
@@ -11,4 +12,22 @@ class Solution:
         x=(diff+sum_xy)//2
         y=sum_xy-x
         return[x,y]
+
+
+#hashmap Approach 
+#time complexity O(2n) space complexity O(n)
+class Solution:
+    def findMissingRepeatingNumbers(self, nums):
+        n=len(nums)
+        freq=[0]*(n+1)
+        for num in nums:
+            freq[num]+=1
+        r=-1
+        m=-1
+        for i in range(1,n+1):
+            if freq[i]==2:
+                r=i
+            elif freq[i]==0:
+                m=i
+        return [r,m]
 
